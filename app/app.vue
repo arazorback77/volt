236a86e54bcb4d8bd1c52ed87e6014e670b4fe07
javanance <javanance@gmail.com>
2025-12-15 21:56:42 +0900

    add volt

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <UButton color="error">Button</UButton>
+    <VoltButton> VoltButton</VoltButton>
+    <VoltAvatar label="P" class="mr-2" size="xlarge" shape="circle"  color="primary"/>
+    <VoltSecondaryButton label="Hello World!!!!2222"></VoltSecondaryButton>
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+import Button from '@/components/volt/SecondaryButton.vue';
+import Avatar from '@/components/volt/Avatar.vue';
+// import AvatarGroup from '@/volt/AvatarGroup.vue';
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  srcDir: 'app/',
   css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui'
-  ]
+  ],
+  vite: {
+    // plugins: [tailwindcss()],
+  },
 
 })
